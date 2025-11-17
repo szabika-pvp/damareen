82f277c09b4db7f90c17a8c5b30d7d407580615a
szabika-pvp <szatomi2008@gmail.com>
2025-11-17 18:32:51 +0100

    removed unneccessary files

--- build/jpackage/Damareen/runtime/bin/Damareen.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set DIR="%~dp0"
-set JAVA_EXEC="%DIR:"=%\java"
-
-
-
-pushd %DIR% & %JAVA_EXEC% %CDS_JVM_OPTS%  -p "%~dp0/../app" -m hu.szatomi.damareen/hu.szatomi.damareen.Main  %* & popd
--- build/libs/run.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-java --module-path runtime/ --add-modules javafx.controls,javafx.fxml -jar damareen-1.0.jar --ui
